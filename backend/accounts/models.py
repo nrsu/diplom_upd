0e535e13c5fcf18b931d118149b3ae506b24d5fa
Makaroshka <sarsenbajmiras@gmail.com>
2025-04-05 20:38:11 +0500

    initial-ml

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -6,6 +6,9 @@ class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
 
+    def __str__(self):
+        return self.name
+
     def to_dict(self):
         return {
             "name": self.name,
@@ -23,6 +26,9 @@ class Product(models.Model):
     colors = models.JSONField()
     sizes = models.JSONField()
     description = models.TextField()
+
+    def __str__(self):
+        return self.name
 
     def save(self, *args, **kwargs):
         if self.discount:  # Проверяем, есть ли скидка
